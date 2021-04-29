cb8885f453f5cb75b474099001a15e7555cb6de6
marcosvillar4 <75869096+marcosvillar4@users.noreply.github.com>
2021-04-29 11:09:34 -0300

    Add files via upload

--- Bat helper.py
+++ Bat helper.py
@@ -1,10 +1,20 @@
 import os
-print ("What is the name of the game?")
+from pathlib import Path  # Import de dependencias
+
+
+print("What is the name of the game?")
 game = input()
-print ("Input the game id (Use the installed games script)")
+print("Input the game id (Use the installed games script)")
 gameid = input()
-print ("Paste the following code in a txt document and change its extension to .bat")
-print ("title Launching " + game)
-print ("echo Launching " + game)
-print ("legendary launch " + gameid)
+p = Path(game + ".bat")
+p.write_text(
+
+    "title Launching "
+    + game
+    + " \recho Launching "
+    + game
+    + " \rlegendary launch "
+    + gameid
+)
+
 os.system("pause")
